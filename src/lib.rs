e45bcaeec90f8e9da75407d94f16893fbc73a653
Ben-PH <benphawke@gmail.com>
2020-08-16 03:00:51 +0200

    Define shared data used for login

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Login {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Register {
+    pub email: String,
+    pub password: String,
+    pub first_name: String,
+    pub last_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
 }
