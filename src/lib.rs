8fb940e6ed2fc8b5ef94c15b2e9eaf47efede2d4
Ben-PH <benphawke@gmail.com>
2020-08-17 08:43:45 +0200

    Use HashSet of Id's to refer to other structs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub type EntryId = u32;
 
@@ -23,54 +23,62 @@ pub struct User {
     pub email: String,
 }
 
+pub type SubjectId = EntryId;
+pub type SubjectCollection = HashMap<EntryId, Subject>;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Subject {
     pub name: String,
-    pub id: EntryId,
+    pub id: SubjectId,
     pub field: Field,
-    pub topics: HashMap<u32, Topic>,
+    pub topics: TopicCollection,
 }
 
 impl Subject {
-    pub fn init(id: EntryId, name: String, field: Field) -> Self {
+    pub fn init(id: SubjectId, name: String, field: Field) -> Self {
         Self{id, name, field, topics: std::collections::HashMap::with_capacity(3)}
     }
 }
+pub type TopicId = EntryId;
+pub type TopicCollection = HashMap<EntryId, Topic>;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Topic {
-    id: EntryId,
+    id: TopicId,
     name: String,
-    learning_objectives: HashMap<u32, String>,
-    pre_req_to: HashMap<u32, Topic>,
-    supported_by: HashMap<u32, Topic>
+    learning_objectives: LearningObjCollection,
+    pre_req_to: HashSet<TopicId>,
+    supported_by: HashSet<TopicId>,
 }
 
 impl Topic {
-    pub fn init(id: EntryId, name: String) -> Self {
+    pub fn init(id: TopicId, name: String) -> Self {
         Self {
             id,
             name,
             learning_objectives: HashMap::new(),
-            pre_req_to: HashMap::new(),
-            supported_by: HashMap::new(),
+            pre_req_to: HashSet::new(),
+            supported_by: HashSet::new(),
         }
     }
 }
 
+pub type LearningObjCollection = HashMap<EntryId, Topic>;
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Field {
     ComputerScience
 }
 
+pub type LearningObjId = EntryId;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct LearningObj {
+    id: LearningObjId,
     name: String,
     instructions: String
 }
 
 impl LearningObj {
-    pub fn init(name: String, instructions: String) -> Self {
+    pub fn init(id: LearningObjId, name: String, instructions: String) -> Self {
         Self {
+            id,
             name,
             instructions
         }
