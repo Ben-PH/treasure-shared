c657e6f3685fe3421da539fe7403498a0393bbb1
Ben-PH <benphawke@gmail.com>
2020-08-29 05:20:14 +0200

    Define data structure for CG weighted graph

--- src/learning_trajectory.rs
+++ src/learning_trajectory.rs
@@ -1,33 +1,39 @@
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
 /// Captures the pathway through a curriculum. A university intro to CS
 /// course might have Sequence, Repetition, and Conditionals as LT's.
-pub struct LearningTrajectory<'a> {
+pub struct LearningTrajectory {
 
     title: String,
     description: String,
     consensus_goals: Vec<ConsensusGoal>,
     learning_goals: Vec<LearningGoal>,
-    edges: Vec<Edge<'a>>,
+    edges: Vec<ConsensusEdge>,
 }
 
-pub struct Edge<'a> {
-    left: &'a dyn Node,
-    right: &'a dyn Node,
-    weight: f32,
+
+pub type CEMap = HashMap<u32, ConsensusEdge>;
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ConsensusEdge {
+    pub label: String,
+    pub left: u32,
+    pub right: u32,
+    pub weight: f32,
 }
 
-pub trait Node {}
 
-impl Node for ConsensusGoal {}
+pub type CGMap = HashMap<u32, ConsensusGoal>;
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ConsensusGoal {
-    delivery: Delivery,
-    comments: String,
-    supporting_eg_LG: String,
-    LearningGoals: Vec<LearningGoal>,
+    pub plugged: bool,
+    pub st8mnt: String,
+    pub weight: f32,
 }
 
+pub type CGGraph = (CGMap, CEMap);
 
 
-impl Node for LearningGoal {}
 /// "Any explicit statement or implicitendorsement of what students can or
 /// should be able to do in relation tocomputational thinking" K. M. Rich
 /// The author of this paper began with collecting a set of these.
@@ -37,6 +43,7 @@ pub struct LearningGoal {
     student_support: f32,
     /// Extent to which this goal is justified through academic theory.
     academic_support: f32,
+    consensus_goal: Option<ConsensusGoal>,
     concept: ConceptCluster,
 }
 
@@ -44,12 +51,4 @@ pub enum ConceptCluster {
     ConditionalLogic,
     FlowControl,
     IterRecParThinking,
-}
-
-
-pub enum Delivery {
-    /// Benefit is only realized when done with a computer. E.g. writing code.
-    Plugged,
-    /// Can be done without technology. E.g. Describing soln.s to algo. puzzles.
-    Unplugged,
 }
